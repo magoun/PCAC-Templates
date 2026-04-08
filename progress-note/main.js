fb71e5e3c41f2a128c25cd476dabce183c6424a9
Creighton Magoun <magoun@gmail.com>
2026-04-07 23:30:17 -0400

    feat: Update textareas to be auto-resizing (shrinking and expanding)

--- progress-note/main.js
+++ progress-note/main.js
@@ -1,13 +1,5 @@
 document.getElementById('dateRecorded').valueAsDate = new Date();
 
-function autoResize() {
-	this.style.height = 'auto';
-	this.style.height = this.scrollHeight + 'px';
-}
-
-textareas = document.querySelectorAll("textarea");
-textareas.forEach(textarea => textarea.addEventListener('input', autoResize, false));
-
 window.addEventListener('beforeunload', function (e) {
 	// Cancel the event
 	e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
